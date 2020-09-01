8c656fd4be43ba7b70e3ca1b5467347d774f6734
DevLeti <devleti0106@gmail.com>
2020-09-01 20:06:05 +0900

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -7,7 +7,7 @@ import json
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 print('start crawl')
-user_name = os.getenv('INPUT_username')
+user_name = os.getenv('$USERNAME')
 print(user_name)
 # req = requests.get('https://github.com/'+user_name)
 # soup = BeautifulSoup()
