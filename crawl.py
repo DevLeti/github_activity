7643eabe4bf738d9e631dc2d43f1d982db58c82a
DevLeti <devleti0106@gmail.com>
2020-09-01 19:19:18 +0900

    added .gitignore, crawl.py, requirements.txt

---
+++ crawl.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
+import os
+import sys
+import json
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+print(BASE_DIR)
+
+# print('start crawl')
+#
+# req = requests.get()
+# soup = BeautifulSoup()
